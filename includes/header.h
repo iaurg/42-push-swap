5c3fa7124a648ab69e8ff032d63b39efbd9ba8a2
Italo aurelio <italoaurelior@gmail.com>
2021-10-24 12:08:40 -0300

    Remove not used headers

--- includes/header.h
+++ includes/header.h
@@ -6,13 +6,11 @@
 /*   By: itaureli <itaureli@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/24 11:34:06 by itaureli          #+#    #+#             */
-/*   Updated: 2021/10/24 11:34:09 by itaureli         ###   ########.fr       */
+/*   Updated: 2021/10/24 12:08:25 by itaureli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
-# include <signal.h>
-# include <sys/types.h>
 # include "../libs/libft/libft.h"
 #endif
