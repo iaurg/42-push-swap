4470e97304a8b01e80242a9b0854c199a950c1c0
Italo aurelio <italoaurelior@gmail.com>
2021-10-25 19:59:34 -0300

    update validation

--- src/validate.c
+++ src/validate.c
@@ -6,7 +6,7 @@
 /*   By: itaureli <itaureli@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/24 13:06:34 by itaureli          #+#    #+#             */
-/*   Updated: 2021/10/25 06:28:05 by itaureli         ###   ########.fr       */
+/*   Updated: 2021/10/25 19:53:44 by itaureli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,35 +22,18 @@ TODO: Not allow duplicates
 // Remove it
 #include <stdio.h>
 
-static int is_space(char c)
-{
-	// Validate spaces and breaklines special
-	// || (c >= 9 && c <= 13)
-    if (c == 32 || (c >= 9 && c <= 13))
-    {
-        return (true);
-    }
-    return (false);
-}
-
 int	is_valid_number(int argc, char **argv)
 {
 	int i;
-	int j;
-	int value;
+	long value;
 
-	j = 0;
 	i = 1;
 	while (i < argc)
 	{
-		value = ft_atoi(argv[i]);
-		if (is_space(*argv[i] + 0)){
-			i++;
-			continue;
-		}
-		printf("value: %d", value);
-		if (!ft_isdigit(value))
+		value = ft_atol(argv[i]);
+		if (value < INT_MIN || value > INT_MAX)
 			return (false);
+		printf("value:%ld\n", value);
 		i++;
 	}
 	return (true);
