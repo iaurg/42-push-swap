ac4e89fc5b8dbc42ce569485ba7752e6e3832ce2
Italo aurelio <italoaurelior@gmail.com>
2021-10-29 20:04:50 -0300

    trying some stacks

--- includes/header.h
+++ includes/header.h
@@ -6,7 +6,7 @@
 /*   By: itaureli <itaureli@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/24 11:34:06 by itaureli          #+#    #+#             */
-/*   Updated: 2021/10/25 06:17:11 by itaureli         ###   ########.fr       */
+/*   Updated: 2021/10/29 20:01:12 by itaureli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,16 @@
 # include <limits.h>
 # include <stdbool.h>
 # include "../libs/libft/libft.h"
+
+typedef struct s_stack
+{
+	int	*numbers;
+	int	size;
+	int	top;
+}	t_stack;
+
+t_stack stack_a();
+t_stack stack_b();
 int	ft_print_error(void);
 int	is_valid_number(int argc, char **argv);
 #endif
