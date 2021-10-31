fc36f45916aa805e195f4daebb421ae0d2340d33
Italo aurelio <italoaurelior@gmail.com>
2021-10-31 16:38:31 -0300

    add new functions

--- includes/header.h
+++ includes/header.h
@@ -6,7 +6,7 @@
 /*   By: itaureli <itaureli@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/24 11:34:06 by itaureli          #+#    #+#             */
-/*   Updated: 2021/10/30 17:16:06 by itaureli         ###   ########.fr       */
+/*   Updated: 2021/10/31 15:28:07 by itaureli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,17 @@
 
 typedef struct s_stack
 {
-	int	*numbers;
+	int		*numbers;
 	int		size;
-	int		top;
+	int		used_size;
 	char	stack_name;
 }	t_stack;
 
-//t_stack	stack_a(char *value);
-// t_stack	stack_b();
 int			ft_print_error(void);
 int			is_valid(char **argv);
 void		init_stack(t_stack *stack, char **values);
 t_stack		*alloc_stack(int argc, char stack_name);
-void		push(t_stack *stack, int value, int position);
+void		push(t_stack *stack_src, t_stack *stack_dst);
+int			is_sorted(t_stack *stack);
+void		swap(t_stack *stack);
 #endif
