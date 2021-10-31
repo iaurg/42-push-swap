156b1d63d002c503d7f037faac278fc5a5132365
Italo aurelio <italoaurelior@gmail.com>
2021-10-31 16:38:59 -0300

    change create logic

--- src/create.c
+++ src/create.c
@@ -6,13 +6,12 @@
 /*   By: itaureli <itaureli@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/30 13:53:58 by itaureli          #+#    #+#             */
-/*   Updated: 2021/10/30 17:43:32 by itaureli         ###   ########.fr       */
+/*   Updated: 2021/10/31 15:28:00 by itaureli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/header.h"
 
-#include <stdio.h>
 void	init_stack(t_stack *stack, char **values)
 {
 	int i;
@@ -22,23 +21,22 @@ void	init_stack(t_stack *stack, char **values)
 	while (i < stack->size)
 	{
 		value = ft_atoi(values[i + 1]);
-		push(stack, value, i);
+		stack->numbers[i] = value;
+		stack->used_size++;
 		i++;
 	}
 }
 
-t_stack	*alloc_stack(int argc, char stack_name)
+t_stack	*alloc_stack(int size, char stack_name)
 {
 	t_stack *stack;
-	int size;
 
 	stack = malloc(sizeof(t_stack));
 	if (!stack)
 		return (NULL);
-	size = argc - 1;
+	stack->used_size = 0;
 	stack->size = size;
 	stack->numbers = malloc(sizeof(int) * size);
 	stack->stack_name = stack_name;
-	stack->top = -1; // empty by comparing TOP == -1
 	return (stack);
 }
