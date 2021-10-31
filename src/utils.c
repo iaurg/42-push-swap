59595bc66173791478c7c8d45e3fc07b686daf2d
Italo aurelio <italoaurelior@gmail.com>
2021-10-31 16:34:51 -0300

    add is sorted into utils

--- src/utils.c
+++ src/utils.c
@@ -6,14 +6,24 @@
 /*   By: itaureli <itaureli@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/30 16:56:01 by itaureli          #+#    #+#             */
-/*   Updated: 2021/10/30 17:21:53 by itaureli         ###   ########.fr       */
+/*   Updated: 2021/10/31 11:55:34 by itaureli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/header.h"
 
-void	push(t_stack *stack, int value, int position)
+int is_sorted(t_stack *stack)
 {
-	stack->numbers[position] = value;
-	stack->top = stack->top + 1;
+	int i;
+
+	i = 1;
+	if(stack->size == 1 || stack->size == 0)
+		return (true);
+	while (i < stack->size)
+	{
+		if (stack->numbers[i - 1] > stack->numbers[i])
+			return (false);
+		i++;
+	}
+	return (true);
 }
