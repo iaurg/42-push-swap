73367bd61a8ab90bc1f78766c6d3c5dba8142ef1
Italo aurelio <italoaurelior@gmail.com>
2021-11-06 13:48:42 -0300

    fix push pushing empty stacks

--- src/push.c
+++ src/push.c
@@ -6,7 +6,7 @@
 /*   By: itaureli <itaureli@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/01 07:27:43 by itaureli          #+#    #+#             */
-/*   Updated: 2021/11/06 13:22:55 by itaureli         ###   ########.fr       */
+/*   Updated: 2021/11/06 13:45:40 by itaureli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 void push(t_stack *source, t_stack *dest, char stack_name)
 {
+	if (source->size < 1)
+		return ;
 	dest->size++;
 	dest->top++;
 	dest->numbers[dest->top] = source->numbers[source->top];
