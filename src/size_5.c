780d19f862978f7dbafec9856462665f829e4830
Italo aurelio <italoaurelior@gmail.com>
2021-11-07 08:28:32 -0300

    fix missing element on stack b

--- src/size_5.c
+++ src/size_5.c
@@ -6,7 +6,7 @@
 /*   By: itaureli <itaureli@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/06 11:48:14 by itaureli          #+#    #+#             */
-/*   Updated: 2021/11/07 08:24:26 by itaureli         ###   ########.fr       */
+/*   Updated: 2021/11/07 08:28:22 by itaureli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,12 +30,7 @@ void	size_5(t_stack *stack_a, t_stack *stack_b)
 		push(stack_a, stack_b, 'b');
 	while (!is_sorted(stack_a))
 		size_3(stack_a);
-	if (is_sorted(stack_b))
-		rotate(stack_b, 'b');
 	push(stack_b, stack_a, 'a');
-	if (stack_b->size > 1)
-	{
-		rotate(stack_a, 'a');
+	if (stack_b->size >= 1)
 		push(stack_b, stack_a, 'a');
-	}
 }
