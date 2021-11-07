c6ec5e6306925414c998493d106ebff83a80287e
Italo aurelio <italoaurelior@gmail.com>
2021-11-07 08:25:45 -0300

    optimize size 5 order

--- src/size_5.c
+++ src/size_5.c
@@ -6,14 +6,26 @@
 /*   By: itaureli <itaureli@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/06 11:48:14 by itaureli          #+#    #+#             */
-/*   Updated: 2021/11/06 13:47:54 by itaureli         ###   ########.fr       */
+/*   Updated: 2021/11/07 08:24:26 by itaureli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/header.h"
 
+// Sort stack with 4 or 5 elements
 void	size_5(t_stack *stack_a, t_stack *stack_b)
 {
+	int smallest;
+
+	smallest = smallest_in_stack(stack_a);
+	move_nbr_top(stack_a, smallest, 'a');
+
+	if (stack_a->size == 5)
+	{
+		push(stack_a, stack_b, 'b');
+		smallest = smallest_in_stack(stack_a);
+		move_nbr_top(stack_a, smallest, 'a');
+	}
 	while (stack_a->size > 3)
 		push(stack_a, stack_b, 'b');
 	while (!is_sorted(stack_a))
@@ -21,12 +33,6 @@ void	size_5(t_stack *stack_a, t_stack *stack_b)
 	if (is_sorted(stack_b))
 		rotate(stack_b, 'b');
 	push(stack_b, stack_a, 'a');
-	if (stack_a->numbers[0] > stack_a->numbers[1])
-	{
-		swap(stack_a, 'a');
-		push(stack_b, stack_a, 'a');
-		return ;
-	}
 	if (stack_b->size > 1)
 	{
 		rotate(stack_a, 'a');
